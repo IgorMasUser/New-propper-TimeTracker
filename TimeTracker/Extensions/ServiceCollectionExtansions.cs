e658914cb230670c8723a68c9fd478f78e50a818
Igor <igormasmas1@gmail.com>
2023-02-05 00:51:47 +0100

    Services moved to extansions class

---
+++ TimeTracker/Extensions/ServiceCollectionExtansions.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using MassTransit;
+
+namespace TimeTracker.Extensions
+{
+    public static class ServiceCollectionExtansions
+    {
+        public static IServiceCollection AddAuthenticationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                                    .GetBytes(configuration.GetSection("JWT:Key").Value)),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = configuration.GetSection("JWT:Issuer").Value,
+                    ValidAudience = configuration.GetSection("JWT:Audience").Value
+                };
+                x.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["accessToken"];
+                        return Task.CompletedTask;
+                    },
+                    OnChallenge = context =>
+                    {
+                        var dataToken = context.Request.Cookies["accessTokenForDataRetriving"];
+                        if (string.IsNullOrEmpty(dataToken))
+                        {
+                            context.Response.Redirect("/Authorization/Authorize");
+                        }
+                        else
+                        {
+                            context.Response.Redirect("/Authorization/Refresh");
+                        }
+                        context.HandleResponse();
+                        return Task.FromResult(0);
+                    }
+
+                };
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddMassTransitServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMassTransit(cfg =>
+            {
+                cfg.SetKebabCaseEndpointNameFormatter();
+                cfg.UsingRabbitMq((cxt, cfg) =>
+                {
+                    cfg.Host("localhost", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    cfg.ConfigureEndpoints(cxt);
+
+                });
+                // cfg.AddRequestClient<SimpleRequest>();
+            });
+
+
+            return services;
+        }
+    }
+}
--- TimeTracker/Program.cs
+++ TimeTracker/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using TimeTracker.Data;
+using TimeTracker.Extensions;
 using TimeTracker.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,52 +16,56 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     ));
 builder.Services.AddScoped<IUserRepo, DBUserRepo>();
 builder.Services.AddTransient<ITokenService, TokenService>();
-builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                        .GetBytes(builder.Configuration.GetSection("JWT:Key").Value)),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidIssuer = builder.Configuration.GetSection("JWT:Issuer").Value,
-        ValidAudience = builder.Configuration.GetSection("JWT:Audience").Value
-    };
-    x.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            context.Token = context.Request.Cookies["accessToken"];
-            return Task.CompletedTask;
-        },
-        OnChallenge = context =>
-        {
-            var dataToken = context.Request.Cookies["accessTokenForDataRetriving"];
-            if (string.IsNullOrEmpty(dataToken))
-            {
-                context.Response.Redirect("/Authorization/Authorize");
-            }
-            else
-            {
-                context.Response.Redirect("/Authorization/Refresh");
-            }
-            context.HandleResponse();
-            return Task.FromResult(0);
-        }
+//builder.Services.AddAuthentication(x =>
+//{
+//    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//}).AddJwtBearer(x =>
+//{
+//    x.RequireHttpsMetadata = false;
+//    x.SaveToken = true;
+//    x.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuerSigningKey = true,
+//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+//                        .GetBytes(builder.Configuration.GetSection("JWT:Key").Value)),
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidateLifetime = true,
+//        ValidIssuer = builder.Configuration.GetSection("JWT:Issuer").Value,
+//        ValidAudience = builder.Configuration.GetSection("JWT:Audience").Value
+//    };
+//    x.Events = new JwtBearerEvents
+//    {
+//        OnMessageReceived = context =>
+//        {
+//            context.Token = context.Request.Cookies["accessToken"];
+//            return Task.CompletedTask;
+//        },
+//        OnChallenge = context =>
+//        {
+//            var dataToken = context.Request.Cookies["accessTokenForDataRetriving"];
+//            if (string.IsNullOrEmpty(dataToken))
+//            {
+//                context.Response.Redirect("/Authorization/Authorize");
+//            }
+//            else
+//            {
+//                context.Response.Redirect("/Authorization/Refresh");
+//            }
+//            context.HandleResponse();
+//            return Task.FromResult(0);
+//        }
 
-    };
-});
+//    };
+//});
 
+var configBuilder = new ConfigurationBuilder();
+IConfiguration configuration = configBuilder.Build();
+
+builder.Services.AddAuthenticationServices(configuration);
+builder.Services.AddMassTransitServices(configuration);
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("RequireRole",
