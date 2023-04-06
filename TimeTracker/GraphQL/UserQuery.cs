b210c4085cf895fd2e45b8f53ab451c9c50e5256
Igor <igormasmas1@gmail.com>
2023-04-06 23:56:50 +0200

    base GraphQL implemented

---
+++ TimeTracker/GraphQL/UserQuery.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using TimeTracker.Data;
+using TimeTracker.Models;
+
+namespace TimeTracker.GraphQL
+{
+    public class UserQuery
+    {
+        private readonly IMapper mapper;
+
+        public UserQuery(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+        public IQueryable<User> GetUsers([Service] ApplicationDbContext context)
+        {
+            //var userDTO = mapper.Map<UserReadDTO>(context.User.ToList());
+
+            return context.User;
+        }
+    }
+}
